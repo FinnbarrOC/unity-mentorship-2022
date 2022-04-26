b51f883901d89ef9b2c10f3d07cade3a4206b16c
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2022-04-26 15:29:04 -0400

    Basic FPS look mechanics

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -7,12 +7,17 @@ public class ExampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xDelta = Input.GetAxis("Mouse X");
+        float yDelta = Input.GetAxis("Mouse Y");
         
+        transform.Rotate(Vector3.up, xDelta);
+        transform.Rotate(-Vector3.right, yDelta);
     }
 }
